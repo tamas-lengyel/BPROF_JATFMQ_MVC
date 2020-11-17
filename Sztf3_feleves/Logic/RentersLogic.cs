2016527f9d4efa9b2d44efb08a959e58a957c837
tamas-lengyel <lengyeltamas97@gmail.com>
2020-11-17 17:18:35 +0100

    Logic class library added with RentersLogic

---
+++ Sztf3_feleves/Logic/RentersLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Models;
+using Repository;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Logic
+{
+    class RentersLogic
+    {
+        IRepository<Renters> renterrepo;
+
+        public RentersLogic(IRepository<Renters> renterrepo)
+        {
+            this.renterrepo = renterrepo;
+        }
+
+        public void DeleteRenter(string renterid)
+        {
+            renterrepo.Delete(renterid);
+        }
+
+        public void InsertRenter(Renters renter)
+        {
+            renterrepo.Insert(renter);
+        }
+
+        public IQueryable<Renters> PrintRenters()
+        {
+            return renterrepo.Print();
+        }
+
+        public void UpdateRenter(string renterid, Renters renter)
+        {
+            renterrepo.Update(renterid, renter);
+        }
+    }
+}
