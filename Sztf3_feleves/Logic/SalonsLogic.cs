72ccb04a2eca4421668084809af3b13f6e697d99
tamas-lengyel <lengyeltamas97@gmail.com>
2020-11-17 17:26:28 +0100

    SalonsLogic added

---
+++ Sztf3_feleves/Logic/SalonsLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Models;
+using Repository;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Logic
+{
+    class SalonsLogic
+    {
+        IRepository<Salons> salonrepo;
+
+        public SalonsLogic(IRepository<Salons> salonrepo)
+        {
+            this.salonrepo = salonrepo;
+        }
+
+        public void DeleteSalon(string salonid)
+        {
+            salonrepo.Delete(salonid);
+        }
+
+        public void InsertSalon(Salons salon)
+        {
+            salonrepo.Insert(salon);
+        }
+
+        public IQueryable<Salons> PrintSalons()
+        {
+            return salonrepo.Print();
+        }
+
+        public void UpdateSalon(string salonid, Salons salon)
+        {
+            salonrepo.Update(salonid, salon);
+        }
+    }
+}
