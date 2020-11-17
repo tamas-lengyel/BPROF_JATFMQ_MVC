997dab413e5f35e318a113d7b33df3c16ed68d9e
tamas-lengyel <lengyeltamas97@gmail.com>
2020-11-17 16:47:27 +0100

    CarsRepository added

---
+++ Sztf3_feleves/Repository/CarsRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Models;
+using Data;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Repository
+{
+    class CarsRepository : IRepository<Cars>
+    {
+        CarRentingDbContext context = new CarRentingDbContext();
+
+        public void Delete(string uid)
+        {
+            var carToDelete = context.Cars.FirstOrDefault(t => t.CarId == uid);
+            context.Cars.Remove(carToDelete);
+            context.SaveChanges();
+        }
+
+        public void Insert(Cars item)
+        {
+            context.Cars.Add(item);
+            context.SaveChanges();
+        }
+
+        public IQueryable<Cars> Print()
+        {
+            return context.Cars.AsQueryable();
+        }
+
+        public void Update(string oldid, Cars newitem)
+        {
+            var oldCar = context.Cars.FirstOrDefault(t => t.CarId == oldid);
+
+            oldCar.Make = newitem.Make;
+            oldCar.Model = newitem.Model;
+            oldCar.ModelYear = newitem.ModelYear;
+            oldCar.BodyType = newitem.BodyType;
+            oldCar.CombFuelEco = newitem.CombFuelEco;
+            oldCar.Available = newitem.Available;
+            oldCar.PricePerDay = newitem.PricePerDay;
+
+            context.SaveChanges();
+        }
+    }
+}
