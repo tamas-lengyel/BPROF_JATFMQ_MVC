b7849d1bc43fb8d40decbce66bbefb2d34bb115b
tamas-lengyel <lengyeltamas97@gmail.com>
2020-11-17 16:01:32 +0100

    Repository class library added with IRepository

---
+++ Sztf3_feleves/Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    interface IRepository<T> where T: new() 
+    {
+        void Insert(T item);
+
+        void Delete(T item);
+
+        void Delete(string uid);
+
+        T FindItem(string uid);
+
+        IQueryable<T> Print();
+
+        void Update(string oldid, T newitem);
+
+        void SaveDb();
+    }
+}
