7837acae5b2617915146c51e2eff1730bca2870d
tamas-lengyel <lengyeltamas97@gmail.com>
2020-11-17 16:54:36 +0100

    SalonsRepository added

---
+++ Sztf3_feleves/Repository/SalonsRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Models;
+using Data;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Repository
+{
+    public class SalonsRepository : IRepository<Salons>
+    {
+        CarRentingDbContext context = new CarRentingDbContext();
+
+        public void Delete(string uid)
+        {
+            var salonToDelete = context.Salons.FirstOrDefault(t => t.SalonId == uid);
+            context.Salons.Remove(salonToDelete);
+            context.SaveChanges();
+        }
+
+        public void Insert(Salons item)
+        {
+            context.Salons.Add(item);
+            context.SaveChanges();
+        }
+
+        public IQueryable<Salons> Print()
+        {
+            return context.Salons.AsQueryable();
+        }
+
+        public void Update(string oldid, Salons newitem)
+        {
+            var oldSalon = context.Salons.FirstOrDefault(t => t.SalonId == oldid);
+
+            oldSalon.PostalCode = newitem.PostalCode;
+            oldSalon.City = newitem.City;
+            oldSalon.Address = newitem.Address;
+
+            oldSalon.Car.Clear();
+            foreach (var item in newitem.Car)
+            {
+                oldSalon.Car.Add(item);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
