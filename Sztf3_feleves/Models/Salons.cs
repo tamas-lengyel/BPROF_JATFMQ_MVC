41d368d63a63dbb1619238cd69b73cdced7bf9dd
tamas-lengyel <lengyeltamas97@gmail.com>
2020-11-17 14:57:28 +0100

    Salons model added to Models

---
+++ Sztf3_feleves/Models/Salons.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class Salons
+    {
+        [Key]
+        public string SalonId { get; set; }
+        [StringLength(30)]
+        public string City { get; set; }
+        [StringLength(50)]
+        public string Address { get; set; }
+        [StringLength(20)]
+        public string PostalCode { get; set; }
+
+        public virtual ICollection<Cars> Car { get; set; }
+    }
+}
