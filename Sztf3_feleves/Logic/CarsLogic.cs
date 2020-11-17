a8e21258cf7722d1b34fcf014e831488e8d13837
tamas-lengyel <lengyeltamas97@gmail.com>
2020-11-17 17:22:29 +0100

    CarsLogic added

---
+++ Sztf3_feleves/Logic/CarsLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Models;
+using Repository;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Logic
+{
+    class CarsLogic
+    {
+        IRepository<Cars> carrepo;
+
+        public CarsLogic(IRepository<Cars> carrepo)
+        {
+            this.carrepo = carrepo;
+        }
+
+        public void DeleteCar(string carid)
+        {
+            carrepo.Delete(carid);
+        }
+
+        public void InsertCar(Cars car)
+        {
+            carrepo.Insert(car);
+        }
+
+        public IQueryable<Cars> PrintCars()
+        {
+            return carrepo.Print();
+        }
+
+        public void UpdateCar(string carid, Cars car)
+        {
+            carrepo.Update(carid, car);
+        }
+    }
+}
